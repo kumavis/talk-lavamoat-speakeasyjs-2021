dfc43b64643f3d7881bdb70b3f3095a0f8c4a2b4
kumavis <aaron@kumavis.me>
2021-01-30 07:22:40 +0800

    Updates

---
+++ precache-manifest.13f5c04c5ef5fccffd4b20035dba0c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8290aee76893cc47e6c",
+    "url": "/talk-lavamoat-speakeasyjs-2021/static/css/main.c1b005b9.chunk.css"
+  },
+  {
+    "revision": "a8290aee76893cc47e6c",
+    "url": "/talk-lavamoat-speakeasyjs-2021/static/js/main.a8290aee.chunk.js"
+  },
+  {
+    "revision": "34c00290c1163fb6037e",
+    "url": "/talk-lavamoat-speakeasyjs-2021/static/css/1.627bd162.chunk.css"
+  },
+  {
+    "revision": "34c00290c1163fb6037e",
+    "url": "/talk-lavamoat-speakeasyjs-2021/static/js/1.34c00290.chunk.js"
+  },
+  {
+    "revision": "b0ba0e8a17dece7f4e02",
+    "url": "/talk-lavamoat-speakeasyjs-2021/static/js/runtime~main.b0ba0e8a.js"
+  },
+  {
+    "revision": "e451a8fc11b5f12f83da519b02da2d89",
+    "url": "/talk-lavamoat-speakeasyjs-2021/index.html"
+  }
+];
